f80a42bbb5d259ff076d020d000424326a96c12a
nolli <nolliepolieollie@gmail.com>
2023-09-20 18:29:39 -0700

    finished with all the server functions. starting to debug.

--- models/thought.js
+++ models/thought.js
@@ -1,5 +1,15 @@
 const mongoose = require('mongoose');
 
+const reactionSchema = new mongoose.Schema({
+    reactionID: {
+        type: mongoose.Types.ObjectId,
+        default: new mongoose.Types.ObjectId()
+    },
+    reactionBody: { type: String, required: true, max: 280, },
+    username: { type: String, required: true, },
+    createdAt: { type: Date, default: Date.now },
+});
+
 const thoughtSchema = new mongoose.Schema({
     thoughtText: { type: String, required: true, min: 1, max: 280, },
     createdAt: { type: Date, default: Date.now },
@@ -7,15 +17,7 @@ const thoughtSchema = new mongoose.Schema({
     reactions: [reactionSchema]
 });
 
-const reactionSchema = new mongoose.Schema({
-    reactionID: {
-        type: ObjectId,
-        default: new mongoose.Types.ObjectId
-    },
-    reactionBody: { type: String, required: true, max: 280, },
-    username: { type: String, required: true, },
-    createdAt: { type: Date, default: Date.now },
-});
+
 
 //virtual
 thoughtSchema.virtual('reactionCount').get(function () {
--- server.js
+++ server.js
@@ -5,9 +5,15 @@ const db = require('./config/connection');
 
 const app = express();
 
-
 const User = require('./models/user');
 const Thought = require('./models/thought');
+
+app.use(express.json()); 
+
+app.use(express.urlencoded({
+    extended: true,
+}
+));
 
 //GETS and PUTS and shit go here. 
 //api/users = GET all users
@@ -35,6 +41,7 @@ app.get('/api/users/:userId', async (req, res) => {
 
 //POST a new user
 app.post('/api/users', async (req, res) => {
+    console.log(req.body); 
     try {
         var user = await User.create(req.body);
         res.status(200).send("user created successfully: " + user._id);
@@ -58,9 +65,10 @@ app.put('/api/users/:userId', async (req, res) => {
             return res.status(404).json({ message: "User no here!" })
         }
 
-        res.status(200).send("yay you did it");
+        res.status(200).json(user);
 
     } catch (err) {
+        console.error(err);
         res.status(500).send({ message: err })
 
     }
@@ -86,7 +94,28 @@ app.delete('/api/users/:userId', async (req, res) => {
 
 app.post('/api/users/:userId/friends/:friendId', async (req, res) => {
     try {
-        // user_id = friend_id 
+        const userId = req.params.userId;
+        const friendId = req.params.friendId;
+
+        //find user by id
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({ message: "No here!" });
+        }
+
+        //find friend by id
+        const friend = await User.findById(friendId);
+        if (!friend) {
+            return res.status(404).json({ message: "No here!" });
+        }
+
+        const userUpdate = await User.findOneAndUpdate(
+            { _id: userId },
+            { $addToSet: { friends: friendId } },
+            { new: true }
+        );
+
+        res.status(200).json({ message: "Friend added!" })
     } catch (err) {
         res.status(500).send({ message: err })
     }
@@ -95,7 +124,19 @@ app.post('/api/users/:userId/friends/:friendId', async (req, res) => {
 
 app.delete('/api/users/:userId/friends/:friendId', async (req, res) => {
     try {
-        //remove friend from User list.  
+        //remove friend from User list. 
+        const userId = req.params.userId;
+        const friendId = req.params.friendId;
+
+        const user = await User.findById(userId);
+
+        const userDelete = await User.findOneAndUpdate(
+            { _id: userId },
+            { $pull: { friends: friendId } },
+            { new: true }
+        );
+
+        res.status(200).json({ message: "fuck that friend." })
     } catch (err) {
         res.status(500).send({ message: err })
     }
@@ -124,7 +165,27 @@ app.get('/api/thoughts/:thoughtId', async (req, res) => {
 
 app.post('/api/thoughts', async (req, res) => {
     try {
-        //create new thought (push created thoughtID to users THOT array)
+        const { thoughtText, username } = req.body;
+
+        //find the user
+        const user = await User.findOne(
+            { username: username }
+        );
+
+        if (!user) {
+            return res.status(404).json({ message: "nO here!" })
+        }
+
+        //create thought
+        const thought = await Thought.create(req.body);
+
+        //Not sure if this works here, but i just think its neat. 
+        // DOESNT WORK: user.FindOneAndUpdate 
+        user.thoughts.push(thought._id);
+        await user.save();
+
+        //respond with created thought
+        res.status(201).json(thought);
     } catch (err) {
         res.status(500).send({ message: err })
     }
@@ -133,7 +194,22 @@ app.post('/api/thoughts', async (req, res) => {
 
 app.put('/api/thoughts/:thoughtId', async (req, res) => {
     try {
-        //update thoughjkfdg hpidaufgh by ID
+        const thoughtId = req.params.thoughtId;
+        const { thoughtText } = req.body;
+
+        //find and update thought by id
+        const thought = await Thought.findByIdAndUpdate(
+            thoughtId,
+            { thoughtText },
+            { new: true }
+        );
+        if (!thought) {
+            return res.status(404).json({ Message: "No THOTS here!" })
+        }
+
+        res.status(200).json(thought);
+
+
     } catch (err) {
         res.status(500).send({ message: err })
     }
@@ -143,6 +219,13 @@ app.put('/api/thoughts/:thoughtId', async (req, res) => {
 app.delete('/api/thoughts/:thoughtId', async (req, res) => {
     try {
         //delete by thoughtId
+        const thoughtID = req.params.thoughtId;
+        const deletedThought = await Thought.findByIdAndDelete(thoughtId);
+
+        if (!deletedThought) {
+            return res.status(404).json({ message: "No THOTS here!" })
+        }
+
     } catch (err) {
         res.status(500).send({ message: err })
     }
@@ -151,7 +234,28 @@ app.delete('/api/thoughts/:thoughtId', async (req, res) => {
 
 app.post('/api/thoughts/:thoughtId/reactions', async (req, res) => {
     try {
-        //create raction stored in thoughts reactions array
+        //create reaction stored in thoughts reactions array
+        const thoughtId = req.params.thoughtId;
+        const { reactionText, username } = req.body;
+
+        //find thought by id
+        const thought = await Thought.findById(thoughtId);
+
+        //create a new reaction object
+        const newReaction = {
+            reactionText,
+            username,
+        };
+
+        //push new reaction
+        thought.reactions.push(newReaction);
+
+        //save
+        await thought.save();
+
+        //respond with updated thought
+        res.status(201).json(thought);
+
     } catch (err) {
         res.status(500).send({ message: err })
     }
@@ -161,6 +265,33 @@ app.post('/api/thoughts/:thoughtId/reactions', async (req, res) => {
 app.delete('/api/thoughts/:thoughtId/reactions/:reactionId', async (req, res) => {
     try {
         //delete reaction by Id
+        const reactionId = req.params.reactionId;
+        const thoughtId = req.params.thoughtId;
+
+        const thought = await Thought.findById(thoughtId);
+        if (!thought) {
+            return res.status(404).json({ Message: "No THOTS here!" })
+        };
+
+        thought.reactions.id(reactionId).deleteOne();
+        // const t = await .findByIdAndDelete(
+        //     { _id: thoughtId },
+        //     { $pull: { reactions : req.params.videoId } },
+        //     { new: true }
+        //   );
+        // for (i = 0; i< thought.reactions.length; i++){
+        //     var thisReact = thought.reactions[i];
+        //     if (thisReact.reactionId == reactionId){
+        //         thought.reactions.pull(thisReact);
+        //         break;
+        //     }
+        // }
+
+        //save
+        await thought.save();
+        // respond 
+        res.status(204).end();
+
     } catch (err) {
         res.status(500).send({ message: err })
     }
